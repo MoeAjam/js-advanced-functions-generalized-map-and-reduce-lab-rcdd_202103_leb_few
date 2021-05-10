bc3a772d9c178f1e1c8d7e3f33393b9601381c6a
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-05-10 17:49:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Add your functions here
+function map(sourceArray,function(a)){
+  let newArr = [];
+  for (let i = 0;i<sourceArray.length;i++){
+    newArr.push(function(sourceArray[i]));
+  }
+}
